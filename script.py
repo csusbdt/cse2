bbf22c5474714f3c783dfdc62eeaa66b47a4ae60
David Turner <csusbdt@gmail.com>
2017-04-27 14:12:43 -0700

    X

--- script.py
+++ script.py
@@ -7,15 +7,11 @@ if platform.python_version_tuple()[0] < '3':
 from pathlib import Path
 import codecs
 
-html = '<html><head></head><body>'
-html += '<h1>CSUSB CSE Flowcharts</h1>'
-html += '<ul>'
-
+# Flowcharts
+html = '<html><head></head><body><h1>CSUSB CSE Flowcharts</h1><ul>'
 for pdf in Path('docs/flowcharts').glob("*.pdf"):
     html += '  <li><a href="' + pdf.stem + '.pdf">' + pdf.stem + '.pdf</li>\n'
-
 html += '</ul></body></html>'
-
 index = 'docs/flowcharts/index.html'
 index = codecs.open(index, "w", encoding="utf-8", errors="xmlcharrefreplace")
 index.write(html)
